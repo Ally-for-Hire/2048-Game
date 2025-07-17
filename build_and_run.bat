7df2cb4a97015b22c7bc445a3c9d8cfc170bf4ca
Ally for Hire <zacyandell@gmail.com>
2025-07-17 18:25:43 -0500

    Optimize build for antivirus compatibility - rename to 2048_game.exe with -O2 -s flags

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,11 +1,11 @@
 @echo off
 echo Building 2048 Game...
-C:\msys64\ucrt64\bin\g++.exe -fdiagnostics-color=always -g main.cpp Window.cpp -o main.exe -lgdi32 -luser32 -lkernel32 -static-libgcc -static-libstdc++
+C:\msys64\ucrt64\bin\g++.exe -fdiagnostics-color=always -O2 -s main.cpp Window.cpp -o 2048_game.exe -lgdi32 -luser32 -lkernel32 -static-libgcc -static-libstdc++
 
 if %ERRORLEVEL% EQU 0 (
     echo Build successful! Running game...
     echo.
-    main.exe
+    2048_game.exe
 ) else (
     echo Build failed!
     pause
